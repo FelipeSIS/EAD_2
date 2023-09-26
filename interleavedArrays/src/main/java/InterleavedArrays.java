81274afe6df7b1ea362b0a55a68242c7d8a041b6
Felipe <felipesisousa@gmail.com>
2023-09-26 19:01:49 -0300

    programa para criação de arrays intercaladas

--- interleavedArrays/src/main/java/InterleavedArrays.java
+++ interleavedArrays/src/main/java/InterleavedArrays.java
@@ -1,2 +1,36 @@
 public class InterleavedArrays {
+    public static void main(String[] args) {
+        int sizeVetor;
+        Scanner entradaValor = new Scanner(System.in);
+        System.out.println("Digite o valor de tamanho");
+        sizeVetor = entradaValor.nextInt();
+
+        int[] A = new int[sizeVetor];
+        int[] B = new int[sizeVetor];
+
+        InterleaveArray valorFinal = new InterleaveArray();
+
+
+        for(int i=0; i < sizeVetor; i++){
+            A[i] = entradaValor.nextInt();
+        }
+
+        for(int i=0; i < sizeVetor; i++){
+            B[i] = entradaValor.nextInt();
+        }
+
+        System.out.println(valorFinal.compute(A, B));
+    }
+
+    String compute(int[] arrayA, int[] arrayB) {
+        String resultado;
+        StringBuilder finalString = new StringBuilder();
+
+        for (int i=0; i < arrayA.length; i++){
+            finalString.append(String.valueOf(arrayA[i] + " " + arrayB[i] + " "));
+        }
+        resultado = finalString.toString();
+        resultado = resultado.substring(0, resultado.length()-1);
+        return resultado;
+    }
 }
