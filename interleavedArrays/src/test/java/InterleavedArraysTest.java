089d0252f8e44241ea749818dfdcf3b6fcda0b82
Felipe <felipesisousa@gmail.com>
2023-09-26 19:05:39 -0300

    criação de rotina de teste

--- interleavedArrays/src/test/java/InterleavedArraysTest.java
+++ interleavedArrays/src/test/java/InterleavedArraysTest.java
@@ -1,3 +1,14 @@
 public class InterleavedArraysTest {
-    
+    @DisplayName("Teste de Strings Intercaladas")
+    @Test
+    void enterTestArrays(){
+
+        int[] A = {1, 2, 3, 4};
+        int[] B={7, 8, 9, 10};
+        int[] C={1, 7, 2, 8, 3, 9, 4, 10};
+
+        final InterleaveArray sut = new InterleaveArray();
+        final String results = sut.compute(A, B);
+        assertThat(results.equals(C.toString()));
+    }
 }
